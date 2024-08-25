5a048701e0a0ff3c3c53b49e4cd915adf9056f96
AlfonsoIng <alfonso1606@yahoo.es>
2024-08-25 13:44:55 -0500

    Actulizacion funciones

--- Condicionales/Ejercicio1.py
+++ Condicionales/Ejercicio1.py
@@ -1,25 +1,21 @@
-#Crear un programa que pida al usuario una letra, y si es vocal, muestre el mensaje "Es vocal". Sino, decirle al usuario que no es vocal
+#Realizar un programa que haga el proceso de formula general para la
+#resolución de ecuaciones, sabiendo que la formula general es la que está en la imagen,
+#el usuario debe ingresar los valores de “a”, “b” y “c”,
+#y el programa debe hacer el proceso para que al final muestre el mensaje:
+#“La solución es: <solucion>”
+#3x^2-5x+2=0    x=1 x=2/3
 
-letra = input("Ingresa una letra: ")
+from math import sqrt
 
-'''if letra.lower() == "a":
-    print("Es una vocal")
+A = int(input("Ingresa el valor de A: "))
+B = int(input("Ingresa el valor de B: "))
+C = int(input("Ingresa el valor de C: "))
+x1 = 0
+x2 = 0
+
+if ((B**2)-(4*A*C)) < 0:
+    print("No se puede realizar porque no se puede sacar raiz a un numero negativo")
 else:
-    if letra.lower() == "e":
-        print("Es una vocal")
-    else:
-        if letra.lower() == "i":
-            print("Es una vocal")
-        else:
-            if letra.lower() == "o":
-                print("Es una vocal")
-            else:
-                if letra.lower() == "u":
-                    print("Es una vocal")
-                else:
-                    print("NO es una vocal")'''
-
-if letra.lower() in "aeiou":
-    print("Es una vocal")
-else:
-    print("NO es una vocal")
+    x1 = (-B + sqrt((B**2)-(4*A*C)))/(2*A)
+    x2 = (-B - sqrt((B**2)-(4*A*C)))/(2*A)
+    print("La solucion es: \nx1=",x1, "\nx2=",x2)
